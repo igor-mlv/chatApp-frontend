463c62a26f79c5766b877f4d0dc161fe4b93efd4
igor-mlv <malov7704@gmail.com>
2024-08-01 13:39:01 -0400

    added useEffect hook for socket event

--- src/app/(userPage)/[userName]/components/userPanel/rooms/Rooms.tsx
+++ src/app/(userPage)/[userName]/components/userPanel/rooms/Rooms.tsx
@@ -4,12 +4,18 @@ import { CircleUserRound } from 'lucide-react';
 import { ScrollArea } from "@/components/ui/scroll-area"
 import socket from '@/lib/socket';
 import { updateUserRooms } from '@/redux/slices/userSlice';
+import React from 'react';
 
 function Rooms() {
     const usedispatch = useDispatch();
-    socket.on("updateRoomsList", (rooms: string[]) => {
-        usedispatch(updateUserRooms(rooms));
-    });
+    React.useEffect(() => {
+        socket.on("updateRoomsList", (rooms: string[]) => {
+            usedispatch(updateUserRooms(rooms));
+        });
+        return () => {
+            socket.off("updateRoomsList");
+        }
+    }, [usedispatch]);
 
     const user = useSelector((state: RootState) => state.user);
     return (
@@ -17,7 +23,7 @@ function Rooms() {
             {user.rooms.map((room) => (
                 <div key={room} className='flex justify-between items-center bg-card border-b-4 border-background px-[20px] py-[10px] rounded-[20px]'>
                     <CircleUserRound size={50} />
-                    <p className='text-[20px]'>{room}</p>
+                    <p className='text-[20px]'>{room.substring(0, 8)}</p>
                 </div>
             ))}
         </ScrollArea>
